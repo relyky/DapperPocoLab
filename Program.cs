02f500abfb186fa3d285d21a048037a2ab6de6a1
Rely Kao <rely_kao@asiavista.com.tw>
2021-07-07 17:22:41 +0800

    commit

--- Program.cs
+++ Program.cs
@@ -2,6 +2,7 @@
 using System.Linq;
 using System.Data.SqlClient;
 using System.Text;
+using System.IO;
 
 namespace DapperPocoLab
 {
@@ -9,62 +10,77 @@ namespace DapperPocoLab
 	{
 		public static void Main()
 		{
-			//Generate All Tables
-			const string indent = "    ";
-			string connStr = "Data Source=RELYNB2;Initial Catalog=MineDB;Integrated Security=True";
-			using (var conn = new SqlConnection(connStr))
+			try
 			{
-				var tableList = DBHelper.GetTableList(conn);
-				tableList.ForEach(table =>
+				//Generate All Tables
+				const string indent = "    ";
+				string connStr = "Data Source=RELYNB2;Initial Catalog=MineDB;Integrated Security=True";
+				string outputFolder = "Output";
+
+				DirectoryInfo outDir = new DirectoryInfo(outputFolder);
+				if (!outDir.Exists) outDir.Create();
+
+				using (var conn = new SqlConnection(connStr))
 				{
-					// Console.WriteLine($"{Utils.JsonSerialize(table, false)}");
-					StringBuilder pocoCode = new StringBuilder();
+					var tableList = DBHelper.GetTableList(conn);
+					tableList.ForEach(table =>
+					{
+						// Console.WriteLine($"{Utils.JsonSerialize(table, false)}");
+						StringBuilder pocoCode = new StringBuilder();
 
-					pocoCode.AppendLine("using System;");
-					pocoCode.AppendLine("using Dapper;");
-					pocoCode.AppendLine("using Dapper.Contrib.Extensions;");
-					pocoCode.AppendLine();
+						pocoCode.AppendLine("using System;");
+						pocoCode.AppendLine("using Dapper;");
+						pocoCode.AppendLine("using Dapper.Contrib.Extensions;");
+						pocoCode.AppendLine();
 
-					pocoCode.AppendLine($"[Table(\"{table.TABLE_NAME}\")]");
-					pocoCode.AppendLine($"public class {table.TABLE_NAME} ");
-					pocoCode.AppendLine("{");
+						pocoCode.AppendLine($"[Table(\"{table.TABLE_NAME}\")]");
+						pocoCode.AppendLine($"public class {table.TABLE_NAME} ");
+						pocoCode.AppendLine("{");
 
-					var columnList = DBHelper.GetColumnList(conn, table.TABLE_NAME);
-					columnList.ForEach(col => {
-						// Console.WriteLine($"{Utils.JsonSerialize(col, false)}");
-
-						string dataType = Utils.GetNetDataType(col.DATA_TYPE);
-						bool isKey = col.IS_PK == "YES";
-						bool isIdentity = col.IS_IDENTITY == "YES";
-						string nullable = col.IS_NULLABLE == "YES" ? "?" : "";
-						string description = col.MS_Description;
-
-						/// summary
-						if (col.MS_Description != null)
+						var columnList = DBHelper.GetColumnList(conn, table.TABLE_NAME);
+						columnList.ForEach(col =>
 						{
-							pocoCode.AppendLine($"{indent}/// <summary>");
-							pocoCode.AppendLine($"{indent}/// {col.MS_Description}");
-							pocoCode.AppendLine($"{indent}/// </summary>");
-						}
+							// Console.WriteLine($"{Utils.JsonSerialize(col, false)}");
 
-						/// Key attribute
-						if (isKey && isIdentity)
-							pocoCode.AppendLine($"{indent}[Key]");
-						if(isKey && !isIdentity)
-							pocoCode.AppendLine($"{indent}[ExplicitKey]");
-						if (!isKey && isIdentity)
-							pocoCode.AppendLine($"{indent}[Computed]");
+							string dataType = Utils.GetNetDataType(col.DATA_TYPE);
+							bool isKey = col.IS_PK == "YES";
+							bool isIdentity = col.IS_IDENTITY == "YES";
+							string nullable = col.IS_NULLABLE == "YES" ? "?" : "";
+							string description = col.MS_Description;
 
-						pocoCode.AppendLine($"{indent}public {dataType} {col.COLUMN_NAME}{nullable} {{ get; set; }}");
+							/// summary
+							if (col.MS_Description != null)
+							{
+								pocoCode.AppendLine($"{indent}/// <summary>");
+								pocoCode.AppendLine($"{indent}/// {col.MS_Description}");
+								pocoCode.AppendLine($"{indent}/// </summary>");
+							}
+
+							/// Key attribute
+							if (isKey && isIdentity)
+								pocoCode.AppendLine($"{indent}[Key]");
+							if (isKey && !isIdentity)
+								pocoCode.AppendLine($"{indent}[ExplicitKey]");
+							if (!isKey && isIdentity)
+								pocoCode.AppendLine($"{indent}[Computed]");
+
+							pocoCode.AppendLine($"{indent}public {dataType} {col.COLUMN_NAME}{nullable} {{ get; set; }}");
+						});
+
+						pocoCode.AppendLine("}");
+						pocoCode.AppendLine();
+
+						// 一個Table一個檔案
+						File.WriteAllText(Path.Combine(outDir.FullName,$"{table.TABLE_NAME}.cs") , pocoCode.ToString(), encoding: Encoding.UTF8);
+						Console.WriteLine(pocoCode.ToString());
 					});
+				}
 
-					pocoCode.AppendLine("}");
-					pocoCode.AppendLine();
-
-					// 一個Table一個檔案
-					Console.WriteLine(pocoCode.ToString());
-				});
-
+				Console.WriteLine();
+				Console.WriteLine("已成功產生 Dapper POCO classes 程式碼，請檢查輸出目錄。");
+			}
+			finally 
+			{
 				Console.WriteLine("Press any key to continue.");
 				Console.ReadKey();
 			}
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
-# DapperPocoLab
- Dapper, POCO classes generator, .NET5
+# C# Dapper POCO類別生成器
+* 支援 Dapper.Contrib
+* IDE: Visual Studio 2019
+* 執行環境: .NET5
