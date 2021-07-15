67b780e63e22507af1d29a626aca84c284eff766
Rely Kao <rely_kao@asiavista.com.tw>
2021-07-15 11:07:57 +0800

    issue 1.1.0.1

---
+++ _issue/Output/品項類別表.cs
@@ -0,0 +1,39 @@
+﻿namespace Vista.DB.Schema
+{
+using System;
+using System.ComponentModel.DataAnnotations;
+using Dapper.Contrib.Extensions;
+using KeyAttribute = Dapper.Contrib.Extensions.KeyAttribute;
+
+[Table("品項類別表")]
+public class 品項類別表 
+{
+    [ExplicitKey]
+    [Required]
+    public string 品項類別 { get; set; }
+    [Required]
+    public string 品項頭碼 { get; set; }
+    /// <summary>
+    /// 狀態: active,disable;
+    /// </summary>
+    [Required]
+    public string 狀態 { get; set; }
+
+    public void Copy(品項類別表 src)
+    {
+        this.品項類別 = src.品項類別;
+        this.品項頭碼 = src.品項頭碼;
+        this.狀態 = src.狀態;
+    }
+
+    public 品項類別表 Clone()
+    {
+        return new 品項類別表 {
+            品項類別 = this.品項類別,
+            品項頭碼 = this.品項頭碼,
+            狀態 = this.狀態,
+        };
+    }
+}
+}
+
---
+++ _issue/Output/計算資產編號.cs
@@ -0,0 +1,40 @@
+﻿namespace Vista.DB.Schema
+{
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using Dapper;
+using Microsoft.Data.SqlClient;
+
+public class 計算資產編號Result 
+{
+    public string 資產編號 { get; set; }
+}
+
+public class 計算資產編號Args 
+{
+    public string 品項類別 { get; set; }
+}
+
+static partial class DBHelperClassExtensions
+{
+public static List<計算資產編號Result> Call計算資產編號(this SqlConnection conn, 計算資產編號Args args)
+{
+    var dataList = conn.Query<計算資產編號Result>("計算資產編號", args,
+        commandType: System.Data.CommandType.StoredProcedure).AsList();
+    return dataList;
+}
+
+public static List<計算資產編號Result> Call計算資產編號(this SqlConnection conn, string 品項類別)
+{
+    var args = new {
+        品項類別,
+    };
+
+    var dataList = conn.Query<計算資產編號Result>("計算資產編號", args,
+        commandType: System.Data.CommandType.StoredProcedure).AsList();
+    return dataList;
+}
+}
+}
+
